b28e971220c28ecc836e245a136cd8e91b8d9f91
Fenres <fenreczz123456@gmail.com>
2025-02-11 18:55:18 +0300

    6

--- app/src/main/java/com/example/tanks/drawers/ElementsDrawer.kt
+++ app/src/main/java/com/example/tanks/drawers/ElementsDrawer.kt
@@ -20,11 +20,41 @@ class ElementsDrawer(val container: FrameLayout) {
     private val elementsOnCoordinate = mutableListOf<Element>()
 
     fun onTouchContainer(x: Float, y: Float) {
-        val topMargin = y.toInt() -(y.toInt() % CELL_SIZE)
+        val topMargin = y.toInt() - (y.toInt() % CELL_SIZE)
         val leftMargin = x.toInt() - (x.toInt() % CELL_SIZE)
         val coordinate = Coordinate(topMargin, leftMargin)
+        if (currentMaterial == Material.EMPTY) {
+            eraseView(coordinate)
+        } else {
+            drawOrReplaceView(coordinate)
+        }
+    }
+
+    private fun drawOrReplaceView(coordinate: Coordinate) {
+        val viewOnCoordinate = getElementByCoordinates(coordinate)
+        if (viewOnCoordinate == null) {
+            drawView(coordinate)
+            return
+        }
+        if (viewOnCoordinate.material != currentMaterial) {
+            replaceView(coordinate)
+        }
+    }
+
+    private fun replaceView(coordinate: Coordinate) {
+        eraseView(coordinate)
         drawView(coordinate)
     }
+
+    private fun eraseView(coordinate: Coordinate) {
+        val elementOnCoordinate = getElementByCoordinates(coordinate)
+        if (elementOnCoordinate != null) {
+            val erasingView = container.findViewById<View>(elementOnCoordinate.viewId)
+            container.removeView(erasingView)
+            elementsOnCoordinate.remove(elementOnCoordinate)
+        }
+    }
+
     fun drawView(coordinate: Coordinate) {
         val view = ImageView(container.context)
         val layoutParams = FrameLayout.LayoutParams(CELL_SIZE, CELL_SIZE)
